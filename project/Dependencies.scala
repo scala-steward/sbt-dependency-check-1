9ec8adf51002f774a22a2df6fb802e9d1618fb35
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-10 19:18:54 +0200

    Update mockito-core to 5.17.0 (#10)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   lazy val munit           = "org.scalameta"           %% "munit"                 % "1.1.0"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.1.0"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.24.3"
-  lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.16.1"
+  lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.17.0"
 }
