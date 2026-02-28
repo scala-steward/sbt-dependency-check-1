2db75ddc3cbc94c38b7f2ce1313464c508a64ea4
Scala Steward <scala_steward@virtuslab.com>
2026-02-28 18:47:13 +0000

    chore(deps): update mockito-core from 5.21.0 to 5.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   lazy val munit           = "org.scalameta"           %% "munit"                 % "1.2.2"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.2.0"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.24.3"
-  lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.21.0"
+  lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.22.0"
 }
