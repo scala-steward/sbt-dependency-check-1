7b2f0a921bbbd6985d7cbe4be58dfe7a56e8b96c
Scala Steward <scala_steward@virtuslab.com>
2026-05-07 15:17:36 +0000

    chore(deps): update log4j-slf4j2-impl from 2.25.4 to 2.26.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   lazy val dependencyCheck = "org.owasp"                % "dependency-check-core" % "12.2.1"
   lazy val munit           = "org.scalameta"           %% "munit"                 % "1.3.0"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.3.0"
-  lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.25.4"
+  lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.26.0"
   lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.23.0"
 }
